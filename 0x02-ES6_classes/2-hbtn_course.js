7bf82cf5bc7e95ab81d56ffdd8c8371efdc4e7a2
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-12-31 14:23:43 +0100

    declared class with members

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,42 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    if (typeof val !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = val;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(val) {
+    if (typeof val !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = val;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(val) {
+    val.forEach((student) => {
+      if (typeof student !== 'string') {
+        throw new TypeError('student must be a string');
+      }
+    });
+    this._students = val;
+  }
+}
