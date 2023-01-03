ebd705972413aa8d6a28b51af9a666409565bcb8
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-01-03 16:50:26 +0100

    added the function name

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
-export default function (getListStudents, city) {
+export default function getStudentsByLocation(getListStudents, city) {
   return getListStudents.filter((x) => (x.location === city));
 }
