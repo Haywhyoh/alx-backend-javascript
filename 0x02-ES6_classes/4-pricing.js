49a6ce750b22645f1ea2d07d96c545b8b88d933b
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-01-07 11:39:49 +0100

    code is working

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,37 @@
+import Currency from "./3-currency";
+export default  class Pricing {
+  constructor  (amount, currency) {
+    this._amount = amount;
+    this._curency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(val) {
+    if (typeof(val) !== "number") {
+        throw new TypeError('code must be a number');
+    }
+    this._amount = val;
+  }
+
+  get currency() {
+    return this._curency;
+  }
+  
+  set amount(val) {
+    if (typeof(val) !== "Currency") {
+        throw new TypeError('code must be a currency');
+    }
+    this._curency = val;
+  }
+
+  displayFullPrice() {
+    return (`${this._amount} ${this._curency.name} ${this._curency.code}`);
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return (amount * conversionRate);
+  }
+}
