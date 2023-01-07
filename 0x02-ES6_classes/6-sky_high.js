20f5586b4d8b787ab7bfba29cf0781e7735e1b29
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-01-07 20:57:45 +0100

    tried again oo

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,18 +3,14 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this.floors = floors;
-  }
-
-  get sqft() {
-    return this._sqft;
+    this._floors = floors;
   }
 
   get floors() {
-    return this.floors;
+    return this._floors;
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
