8392251baf5ac1870315551fcc7bc65dee6fcd2e
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-01-07 11:49:06 +0100

    Implemented a class named Pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,6 +1,7 @@
-import Currency from "./3-currency";
-export default  class Pricing {
-  constructor  (amount, currency) {
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
     this._amount = amount;
     this._curency = currency;
   }
@@ -10,8 +11,8 @@ export default  class Pricing {
   }
 
   set amount(val) {
-    if (typeof(val) !== "number") {
-        throw new TypeError('code must be a number');
+    if (typeof (val) !== 'number') {
+      throw new TypeError('code must be a number');
     }
     this._amount = val;
   }
@@ -19,10 +20,10 @@ export default  class Pricing {
   get currency() {
     return this._curency;
   }
-  
-  set amount(val) {
-    if (typeof(val) !== "Currency") {
-        throw new TypeError('code must be a currency');
+
+  set currency(val) {
+    if (val instanceof Currency) {
+      throw new TypeError('code must be a currency');
     }
     this._curency = val;
   }
